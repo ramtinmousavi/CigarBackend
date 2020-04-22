773539a5bc0a50a9a08bf4381f82de3a5f766358
ramtinmousavi <ramtin.mousavi@ymail.com>
2020-04-22 15:10:50 +0430

    update configuration

---
+++ Cigar/__init__.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:rm3241365@localhost/cigardb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+DataBase = SQLAlchemy(app)
+MarshMallow = Marshmallow (app)
+
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User_Model.query.get(user_id)
+
+#import user User_Model
+#import all controllers
+
+#register their blueprint
