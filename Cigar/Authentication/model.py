59f56a3c7ec9d884db91e26f9a6abcae57a4502d
ramtinmousavi <ramtin.mousavi@ymail.com>
2020-04-23 21:51:08 +0430

    add many to many relation between media and users

--- Cigar/Authentication/model.py
+++ Cigar/Authentication/model.py
@@ -6,8 +6,36 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from Cigar import MarshMallow as ma
 from flask_marshmallow import Marshmallow
 
+from Cigar.Multimedia.model import Category, Video, Book, Podcast, Motivation
+
 from sqlalchemy.orm import validates
 import re
+
+
+#many to many relationship between users and videos
+user_video_table = db.Table ('user_video_table',
+db.Column('user_id', db.Integer, db.ForeignKey('video_model.id')),
+db.Column('video_id', db.Integer, db.ForeignKey('user_model.id'))
+)
+
+#many to many relationship between users and books
+user_book_table = db.Table ('user_book_table',
+db.Column('user_id', db.Integer, db.ForeignKey('book_model.id')),
+db.Column('book_id', db.Integer, db.ForeignKey('user_model.id'))
+)
+
+#many to many relationship between users and podcasts
+user_podcast_table = db.Table ('user_podcast_table',
+db.Column('user_id', db.Integer, db.ForeignKey('podcast_model.id')),
+db.Column('podcast_id', db.Integer, db.ForeignKey('user_model.id'))
+)
+
+#many to many relationship between users and motivations
+user_motivation_table = db.Table ('user_motivation_table',
+db.Column('user_id', db.Integer, db.ForeignKey('motivation_model.id')),
+db.Column('motivation_id', db.Integer, db.ForeignKey('user_model.id'))
+)
+
 
 class User (db.Model, UserMixin):
     __tablename__ = 'user_model'
@@ -16,6 +44,11 @@ class User (db.Model, UserMixin):
     name =  db.Column(db.String(50), nullable = False)
     email = db.Column(db.String(50), unique=True , nullable = False)
     pass_hash = db.Column(db.Text)
+    viewed_videos = db.relationship("Video", secondary = user_video_table)
+    viewed_books = db.relationship("Book", secondary = user_book_table)
+    viewed_podcasts = db.relationship("Podcast", secondary = user_podcast_table)
+    viewed_motivations = db.relationship("Motivation", secondary = user_motivation_table)
+
 
     @validates ('email')
     def validate_email (self, key, value):
@@ -24,15 +57,31 @@ class User (db.Model, UserMixin):
 
     def __init__ (self, name, email, password):
         self.name = name
-        self.email = email
+        self.email = email.lower()
         self.pass_hash = generate_password_hash (password)
+
+    def check_password (self, password):
+        return check_password_hash (self.pass_hash,password)
 
     def save (self):
         db.session.add (self)
         db.session.commit()
 
-    def check_password (self, password):
-        return check_password_hash (self.pass_hash,password)
+    def append_viewed_video (self, video):
+        self.viewed_videos.append (video)
+        db.session.commit()
+
+    def append_viewed_book (self, book):
+        self.viewed_books.append (book)
+        db.session.commit()
+
+    def append_viewed_podcast (self, podcast):
+        self.viewed_podcasts.append (podcast)
+        db.session.commit()
+
+    def append_viewed_motivation (self, motivation):
+        self.viewed_motivations.append (motivation)
+        db.session.commit()
 
     @staticmethod
     def query_by_email (email):
