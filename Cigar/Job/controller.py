ee86b961604f866f349eee30695a4738e05a7135
ramtinmousavi <ramtin.mousavi@ymail.com>
2020-04-26 20:00:31 +0430

    define scheduler to update user motivations

---
+++ Cigar/Job/controller.py
@@ -0,0 +1,12 @@
+import schedule
+import time
+import datetime
+
+from Cigar.Authentication.model import User
+
+def run_schedule ():
+    schedule.every().day.at("00:00").do(User.update_to_show_motivations)
+    schedule.every().day.at("12:00").do(User.update_reserve_motivations)
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
