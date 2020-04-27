a430b0cd2c65076e3e9d1777aede3f36e9172ca2
ramtinmousavi <ramtin.mousavi@ymail.com>
2020-04-28 02:16:06 +0430

    creating Video APIs for admin

--- Cigar/Admin/controller.py
+++ Cigar/Admin/controller.py
@@ -78,16 +78,16 @@ admin.add_url_rule('/api/editMotivation/<int:motivationId>' , view_func = edit_m
 
 @cross_origin(supports_credentials=True)
 @login_required
-@Admin_Required (['motivation'])
+@Admin_Required ([])
 def delete_motivation (motivationId):
 
     current_motivation = Motivation.query.get (int(motivationId))
     if (current_motivation is not None):
         current_motivation.delete()
-        output = {'motivation':'', 'status':'OK'}
+        output = {'status':'OK'}
         return jsonify (output)
 
-    output = {'motivation':'', 'status':'motivation id is wrong'}
+    output = {'status':'motivation id is wrong'}
     return jsonify (output)
 
 admin.add_url_rule('/api/deleteMotivation/<int:motivationId>' , view_func = delete_motivation)
@@ -121,3 +121,100 @@ def get_all_motivations ():
     output = {'motivations': Motivation.serialize_many(motivations), 'status':'OK'}
 
 admin.add_url_rule('/api/getAllMotivations/' , view_func = get_all_motivations)
+
+#------------------------------------------------------#
+
+@cross_origin(supports_credentials=True)
+@login_required
+@Admin_Required (['video'])
+def add_video (categoryId):
+    if request.method == 'POST':
+        req = request.get_json(force = True)
+
+        title = req['title']
+        description = req['description']
+        url = req['url']
+        new_video = Video (title, description, utrl, int(categoryId))
+        new_video.save()
+
+        output = {'video':new_video.serialize_one(), 'status':'OK'}
+        return jsonify (output)
+
+    output = {'motivation':'', 'status':'method is not POST'}
+    return jsonify (output)
+
+admin.add_url_rule('/api/addVideo/<int:categoryId>' , view_func = add_video, methods = ['POST' , 'GET'])
+
+
+@cross_origin(supports_credentials=True)
+@login_required
+@Admin_Required (['video'])
+def edit_video (videoId):
+    if request.method == 'POST':
+        req = request.get_json(force = True)
+
+        current_video = Video.query.get (int(videoId))
+        if (current_video is not None):
+
+            title = req['title']
+            description = req['description']
+            url = req ['url']
+            current_video.edit (title, description, url)
+
+            output = {'video':current_video.serialize_one(), 'status':'OK'}
+            return jsonify (output)
+
+        output = {'video':'', 'status':'video id is wrong'}
+        return jsonify (output)
+
+    output = {'video':'', 'status':'method is not POST'}
+    return jsonify (output)
+
+admin.add_url_rule('/api/editVideo/<int:videoId>' , view_func = edit_video, methods = ['POST' , 'GET'])
+
+
+@cross_origin(supports_credentials=True)
+@login_required
+@Admin_Required ([])
+def delete_video (videoId):
+
+    current_video = Video.query.get (int(videoId))
+    if (current_video is not None):
+        current_video.delete()
+        output = {'status':'OK'}
+        return jsonify (output)
+
+    output = {'status':'video id is wrong'}
+    return jsonify (output)
+
+admin.add_url_rule('/api/deleteVideo/<int:videoId>' , view_func = delete_video)
+
+
+@cross_origin(supports_credentials=True)
+@login_required
+@Admin_Required (['video'])
+def get_motivation (videoId):
+
+    current_video = Video.query.get (int(videoId))
+    if (current_video is not None):
+
+        output = {'video':current_video.serialize_one(), 'status':'OK'}
+        return jsonify (output)
+
+    output = {'video':'', 'status':'video id is wrong'}
+    return jsonify (output)
+
+
+admin.add_url_rule('/api/getVideo/<int:videoId>' , view_func = get_video)
+
+
+@cross_origin(supports_credentials=True)
+@login_required
+@Admin_Required (['videos'])
+def get_all_videos ():
+
+    videos = Video.query.all()
+
+    output = {'videos': Video.serialize_many(videos), 'status':'OK'}
+
+admin.add_url_rule('/api/getAllVideos/' , view_func = get_all_videos)
