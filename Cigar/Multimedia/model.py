f8a4926af78592182fd53ce99461545be23e370b
ramtinmousavi <ramtin.mousavi@ymail.com>
2020-04-28 02:30:11 +0430

    adding edit and delete methods to Podcast model

--- Cigar/Multimedia/model.py
+++ Cigar/Multimedia/model.py
@@ -157,6 +157,16 @@ class Podcast (db.Model):
         db.session.add (self)
         db.session.commit()
 
+    def edit (self, title, description, url):
+        self.title = title
+        self.description = description
+        self.url = url
+        db.session.commit()
+
+    def delete (self):
+        db.session.delete (self)
+        db.session.commit()
+
     def serialize_one (self):
         return PodcastSchema().dump(self)
 
