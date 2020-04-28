353201aed412d78a9f33a12939eef4bedeb85e57
ramtinmousavi <ramtin.mousavi@ymail.com>
2020-04-28 20:27:50 +0430

    adding get_media_by_category API

--- Cigar/Multimedia/controller.py
+++ Cigar/Multimedia/controller.py
@@ -13,8 +13,6 @@ multimedia = Blueprint('multimedia', __name__)
 @cross_origin(supports_credentials=True)
 @login_required
 def get_media (mediaType=None):
-    user = User.query.get (session['user_id'])
-
     if mediaType:
         if int (mediaType) == 1:
             media = Video.query.all()
@@ -30,12 +28,17 @@ def get_media (mediaType=None):
             media = Podcast.query.all()
             output = {'podcasts':Podcast.serialize_many(media), 'status':'OK'}
             return jsonify (output)
+
+        else:
+            output = {'status':'wrong media type'}
+            return jsonify (output)
     #show all multimedias
     else:
         output = {'podcasts':Podcast.serialize_many(Podcast.query.all()),
                     'books':Book.serialize_many(Book.query.all()),
                     'videos':Video.serialize_many(Video.query.all()),
                     'status':'OK'}
+        return jsonify (output)
 
 multimedia.add_url_rule('/api/getMedia/<int:mediaType>' , view_func = get_media)
 
@@ -49,3 +52,34 @@ def get_motivations (count = 10):
     return jsonify (output)
 
 multimedia.add_url_rule('/api/getMotivations/<int:count>' , view_func = get_motivations)
+
+
+@cross_origin(supports_credentials=True)
+@login_required
+def get_media_by_category(categoryId, mediaType=None):
+    category = Category.query.get (int(categoryId))
+    if category:
+        if mediaType:
+            if int(mediaType) == 1:     #if video
+                output = {'videos':Video.serialize_many(category.videos), 'status':'OK'}
+                return jsonify (output)
+            elif int(mediaType) == 2:   #if book
+                output = {'books':Book.serialize_many(category.books), 'status':'OK'}
+                return jsonify(output)
+            elif int(mediaType) == 3:   #if podcast
+                output = {'podcasts':Podcast.serialize_many(category.podcasts), 'status':'OK'}
+                return jsonify (output)
+
+            output = {'status':'wrong media type'}
+            return jsonify (output)
+
+        output = {'videos':Video.serialize_many(category.videos),
+                    'books':Book.serialize_many(category.books),
+                    'podcasts':Podcast.serialize_many(category.podcasts),
+                    'status':'OK'}
+        return jsonify (output)
+
+    output = {'status':'wrong category id'}
+    return jsonify (output)
+
+multimedia.add_url_rule('/api/getMediaByCategory/<int:categoryId>/<int:mediaType>' , view_func = get_media_by_category)
