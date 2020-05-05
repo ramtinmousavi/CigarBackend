69a76e67dd0cd37679724f51c389ac7501e65d53
ramtinmousavi <ramtin.mousavi@ymail.com>
2020-05-05 14:18:57 +0430

    edit Authentication APIs

--- Cigar/Authentication/controller.py
+++ Cigar/Authentication/controller.py
@@ -2,6 +2,7 @@ from flask import request, jsonify, session, Blueprint
 from flask_login import login_required, login_user, logout_user
 from flask_cors import  cross_origin
 
+from Cigar import response_generator
 from Cigar.Authentication.model import User
 
 import re
@@ -21,19 +22,19 @@ def sign_up():
         if (re.search ('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', email)):
 
             if (User.query_by_email (email) is not None):
-                output = {'user':'', 'status':'user already exists'}
+                output = response_generator (None, 304, 'ایمیل تکراری است')
                 return jsonify (output)
 
             new_user = User (name, email, password)
             new_user.save()
 
-            output = {'user':new_user.serialize_one(), 'status':'OK'}
+            output = response_generator (new_user.serialize_one(), 200, 'ثبت نام با موفقیت انجام شد')
             return jsonify (output)
 
-        output = {'user':'', 'status':'wrong email format'}
+        output = response_generator (None, 304, 'لطفا ایمیل را به درستی وارد کنید')
         return jsonify(output)
 
-    output = {'user':'', 'status':'method is not POST'}
+    output = response_generator (None, 405, 'method is not POST')
     return jsonify (output)
 
 authentication.add_url_rule('/api/signup' , view_func = sign_up, methods = ['POST' , 'GET'])
@@ -53,18 +54,18 @@ def login():
             session ['user_id'] = stored_user.id
             session ['role'] = stored_user.role
 
-            output = {'user': stored_user.serialize_one(), 'status':'OK'}
+            response_generator (stored_user.serialize_one(), 200, 'ورود با موفقیت انجام شد')
             return jsonify (output)
 
         else:
             if stored_user is None:
-                output = {'user':'', 'status':'user not found'}
+                output = response_generator (None, 401, 'لطفا ایمیل و گذرواژه را به درستی وارد نمایید')
                 return jsonify (output)
             elif not stored_user.check_password(req['password']):
-                output = {'user':'', 'status':'password incorrect'}
+                output = response_generator (None, 401, 'لطفا ایمیل و گذرواژه را به درستی وارد نمایید')
                 return jsonify(output)
 
-    output = {'user':'', 'status':'method is not POST'}
+    output = response_generator (None, 405, 'method is not POST')
     return jsonify (output)
 
 authentication.add_url_rule('/api/login' , view_func = login, methods = ['POST' , 'GET'])
@@ -78,7 +79,7 @@ def logout():
     logout_user()
 
     user = User.query.get(user_id)
-    output = {'user':user.serialize_one(), 'status':'OK'}
+    output = response_generator (user.serialize_one(), 200, 'شما با موفقیت خارج شدید')
     return jsonify (output)
 
 authentication.add_url_rule('/api/logout' , view_func = logout)
@@ -93,10 +94,10 @@ def change_password ():
         new_pw = req['password']
         user.change_password (new_pw)
 
-        output = {'user':user.serialize_one(), 'status':'OK'}
+        output = response_generator (user.serialize_one(), 200, 'تغییر گذرواژه با موفقیت انجام شد')
         return jsonify (output)
 
-    output = {'user':user.serialize_one(), 'status':'method is not POST'}
+    output = response_generator (None, 405, 'method is not POST')
     return jsonify (output)
 
 authentication.add_url_rule('/api/changePassword' , view_func = change_password, methods = ['POST' , 'GET'])
@@ -112,10 +113,10 @@ def rename ():
         new_name = req['name']
         user.rename (new_name)
 
-        output = {'user':user.serialize_one(), 'status':'OK'}
+        output = response_generator (user.serialize_one(), 200, 'تغییر نام با موفقیت انجام شد')
         return jsonify (output)
 
-    output = {'user':user.serialize_one(), 'status':'method is not POST'}
+    output = response_generator (None, 405, 'method is not POST')
     return jsonify (output)
 
 authentication.add_url_rule('/api/rename' , view_func = rename, methods = ['POST' , 'GET'])
@@ -128,10 +129,10 @@ def edit_motivation_count (count):
         user = User.query.get (session['user_id'])
         user.edit_count (int (count))
 
-        output = {'user':user.serialize_one(), 'status':'OK'}
+        output = response_generator (user.serialize_one(), 200, 'تغییر با موفقیت اعمال شد')
         return jsonify (output)
 
-    output = {'user':user.serialize_one(), 'status':'wrong count'}
+    output = response_generator (None, 304, 'تعداد وارد شده بیش از سقف مجاز می باشد')
     return jsonify (output)
 
 authentication.add_url_rule('/api/editMotivationCount/<int:count>' , view_func = edit_motivation_count)
