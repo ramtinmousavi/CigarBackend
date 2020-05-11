9cf39b31f531ca70be9a423262693967ac331a2d
ramtinmousavi <ramtin.mousavi@ymail.com>
2020-05-11 14:11:11 +0430

    remove Motivation related APIs from Multimedia package

--- Cigar/Multimedia/controller.py
+++ Cigar/Multimedia/controller.py
@@ -2,11 +2,9 @@ from flask import request, jsonify, session, Blueprint
 from flask_login import login_required
 from flask_cors import  cross_origin
 
-from Cigar.Multimedia.model import Category, Video, Book, Podcast, Motivation
-from Cigar.Authentication.model import User
+from Cigar.Multimedia.model import Video, Book, Podcast
 from Cigar import response_generator
 
-import random
 
 multimedia = Blueprint('multimedia', __name__)
 
@@ -44,36 +42,3 @@ def get_media (mediaType=None):
 
 multimedia.add_url_rule('/api/getMedia/<int:mediaType>' , view_func = get_media)
 multimedia.add_url_rule('/api/getMedia' , view_func = get_media)
-
-
-
-@cross_origin(supports_credentials=True)
-@login_required
-def get_motivations (subcategoryId):
-    user = User.query.get (session['user_id'])
-    motivations = user.get_to_show_motivations(subcategoryId)
-
-    output = response_generator (Motivation.serialize_many (motivations), 200, 'OK')
-    return jsonify (output)
-
-multimedia.add_url_rule('/api/getMotivations/<int:subcategoryId>' , view_func = get_motivations)
-
-
-@cross_origin(supports_credentials=True)
-@login_required
-def get_category (categoryId = None):
-    if categoryId:
-        category = Category.query.get(int(categoryId))
-        if (category):
-            output = response_generator (category.serialize_one(), 200, 'OK')
-            return jsonify (output)
-
-        output = response_generator (None, 406, 'wrong category id')
-        return jsonify (output)
-
-    categories = Category.query.all()
-    output = response_generator (Category.serialize_many (categories), 200, 'OK')
-    return jsonify (output)
-
-multimedia.add_url_rule('/api/getCategory/<int:categoryId>' , view_func = get_category)
-multimedia.add_url_rule('/api/getCategory' , view_func = get_category)
