1093d1c93488efead11b668adea17d0c7e0792bd
ramtinmousavi <ramtin.mousavi@ymail.com>
2020-07-09 15:46:36 +0430

    web server config

--- instances.py
+++ instances.py
@@ -4,30 +4,24 @@ from Cigar.Authentication.model import User
 from Cigar.Multimedia.model import Video, Book, Podcast
 from Cigar.Motivation.model import Category, SubCategory, Motivation, UserMotivation
 
-vid = ['1.mp4', '2.mp4']
-book = ['1.pdf']
-pod = ['1.mp3', '2.mp3', '3.mp3']
-img = []
-for i in range (1,11):
-    img.append (str(i)+ '.jpg')
 
 def make_instances ():
     db.create_all()
-    User ('Admin','admin','admin3241365','owner').save()
+    User ('admin','admin','admin','owner').save()
 
-    for i in range (1,51):
-        Video (str('ویدیو '+str(i)), str('توضیحات برای ویدیو '+ str(i)), vid [i%2]).save()
-        Book (str('کتاب '+str(i)), str('توضیحات برای کتاب '+ str(i)), book [0]).save()
-        Podcast (str('پادکست '+str(i)), str('توضیحات برای پادکست '+ str(i)), pod [i%3]).save()
+    for i in range (1,101):
+        Video (str('Video'+str(i)), str('Description For Video '+ str(i)), 'google.com').save()
+        Book (str('Book'+str(i)), str('Description For Book '+ str(i)), 'google.com').save()
+        Podcast (str('Podcast'+str(i)), str('Description For Podcast '+ str(i)), 'google.com').save()
 
     for i in range (1,6):
-        Category (str('دسته بندی شماره '+ str(i))).save()
+        Category (str('Category #'+ str(i))).save()
         for j in range (1,11):
-            SubCategory (str('زیردسته شماره '+str(j)+' از دسته بندی شماره '+str(i)), img [j-1], i).save()
+            SubCategory (str('Subcategory #'+str(j)+' From Category #'+str(i)), 'google.com', i).save()
 
     for x in range (1,51):
-        for y in range (1,51):
-            Motivation(str('پیام شماره '+str(y)+ ' از زیردسته شماره '+str(x)),x).save()
+        for y in range (1,101):
+            Motivation(str('Motivation #'+str(y)+ 'For SubCategory #'+str(x)),x).save()
 
 if __name__ == '__main__' :
     make_instances()
--- manage.py
+++ manage.py
@@ -1,6 +1,36 @@
+from Cigar import DataBase as db
 from Cigar import app
-import os
+from instances import make_instances
+
+from flask_script import Manager, prompt_bool
+
+manager = Manager(app)
+
+@manager.command
+def initdb():
+    db.create_all()
+    make_instances()
+    print ('Initialized the database')
+
+#ask password to drop db
+@manager.command
+def dropdb():
+    if prompt_bool(
+        "Are you sure you want to lose all your data"):
+        db.drop_all()
+        print ('Dropped the database')
+
+"""
+@manager.command
+def run():
+    app.secret_key = os.urandom(12)
+
+    t = Thread(target = run_schedule)
+    t.daemon = True
+    t.start()
+
+    app.run(debug = True, host='0.0.0.0')
+"""
 
 if __name__ == '__main__':
-    app.secret_key = os.urandom (12)
     app.run()
